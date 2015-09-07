741566276b373769594378ff3df5c43d6c75b45e
danikf <daniel.frantik@gmail.com>
2015-09-08 00:13:03 +0200

    TikEntityMetadata comments.

--- tik4net.objects/TikEntityMetadata.cs
+++ tik4net.objects/TikEntityMetadata.cs
@@ -7,26 +7,63 @@ using System.Reflection;
 
 namespace tik4net.Objects
 {
+    /// <summary>
+    /// Metadata of one mikrotik entity (scaned via reflection of entity class and its attributes).
+    /// Entity class must be decorated by <seealso cref="TikEntityAttribute"/> and every managed property
+    /// should be decoraded by <seealso cref="TikPropertyAttribute"/>.
+    /// </summary>
+    /// <seealso cref="TikEntityAttribute"/>
+    /// <seealso cref="TikPropertyAttribute"/>
+    /// <seealso cref="TikEntityMetadataCache"/>
     public class TikEntityMetadata
     {
         private Dictionary<string, TikEntityPropertyAccessor> _properties;
 
+        /// <summary>
+        /// All properties of the entity which are decorated by <seealso cref="TikPropertyAttribute"/>
+        /// </summary>
         public IEnumerable<TikEntityPropertyAccessor> Properties
         {
             get { return _properties.Values; }
-        }        
+        }
 
+        /// <summary>
+        /// entity path in API notation (e.q. /system/resource)
+        /// </summary>
+        /// <seealso cref="TikEntityAttribute.EntityPath"/>
         public string EntityPath { get; private set; }
+
+        /// <summary>
+        /// If the whole entity is R/O.
+        /// </summary>
+        /// <seealso cref="TikEntityAttribute.IsReadOnly"/>
         public bool IsReadOnly { get; private set; }
+
+        /// <summary>
+        /// If =detail= option should be used during entity load.
+        /// </summary>
+        /// <seealso cref="TikEntityAttribute.IncludeDetails"/>
         public bool IncludeDetails { get; private set; }
 
+        /// <summary>
+        /// If all <see cref="Properties"/> should be explicitly listed via .proplist option.
+        /// </summary>
+        /// <seealso cref="TikEntityAttribute.IncludeProplist"/>
         public bool IncludeProplist { get; private set; }
 
+        /// <summary>
+        /// The .id property of the entity or null (if no property is decorated by <see cref="TikPropertyAttribute.FieldName"/> = .id).
+        /// </summary>
         public TikEntityPropertyAccessor IdProperty
         {
             get { return GetPopertyDescriptor(TikSpecialProperties.Id); }
         }
 
+        /// <summary>
+        /// .ctor. Performs reflection scan ot given entity type and its properties.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <remarks>Slow operation.</remarks>
         public TikEntityMetadata(Type entityType)
         {
             TikEntityAttribute entityAttribute = (TikEntityAttribute)entityType.GetCustomAttributes(true).FirstOrDefault(a => a is TikEntityAttribute);
@@ -45,7 +82,7 @@ namespace tik4net.Objects
                 .ToDictionary(propDescriptor => propDescriptor.FieldName);                
         }
 
-        public TikEntityPropertyAccessor GetPopertyDescriptor(string propertyName)
+        private TikEntityPropertyAccessor GetPopertyDescriptor(string propertyName)
         {
             return _properties[propertyName];
         }
