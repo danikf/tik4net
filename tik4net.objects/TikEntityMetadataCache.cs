016601ff870c4a8977d74c0d89ddb295122c2de8
danikf <daniel.frantik@gmail.com>
2015-09-08 00:04:59 +0200

    TikEntityMetadataCache comments.

--- tik4net.objects/TikEntityMetadataCache.cs
+++ tik4net.objects/TikEntityMetadataCache.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace tik4net.Objects
 {
+    /// <summary>
+    /// Cache for extracted metadata about mikrotik entities for entity mapper.
+    /// Main reason is to improve performance via caching slow reflection operations.
+    /// </summary>
+    /// <remarks>Cache is thread-safe.</remarks>
+    /// <seealso cref="TikEntityAttribute"/>
+    /// <seealso cref="TikPropertyAttribute"/>
+    /// <seealso cref="TikEntityMetadata"/>
     public static class TikEntityMetadataCache
     {
         private static readonly object _lockObj = new object();
         private static Dictionary<Type, TikEntityMetadata> _cache = new Dictionary<Type, TikEntityMetadata>();
 
+        /// <summary>
+        /// Gets (or creates new) <typeparamref name="TEntity"/> metadata via reflection of its attributes.
+        /// </summary>
+        /// <typeparam name="TEntity">Type of the entity.</typeparam>
+        /// <returns>Entity metadata used by entity mapper.</returns>
         public static TikEntityMetadata GetMetadata<TEntity>()
         {
             Type key = typeof(TEntity);
