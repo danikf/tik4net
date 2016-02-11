0f1a27e994db598776dc7510855caa44270e514c
David Bullock <david@invertedtomato.com>
2016-02-11 11:25:55 +1000

    Added a timespan util to convert from MikroTik timespans to useable formats

---
+++ tik4net.tests/Utilities/MTikTimeUtilTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VLife.Choreographer.Utilities.TestMethods {
+    [TestClass]
+    public class IntToTimeStringTestMethods {
+        [TestMethod]
+        public void FromTestMethod_None() {
+            Assert.AreEqual("none", MTikTimeUtil.ToMTikTime(0));
+        }
+        [TestMethod]
+        public void FromTestMethod_Second() {
+            Assert.AreEqual("10s", MTikTimeUtil.ToMTikTime(10));
+        }
+        [TestMethod]
+        public void FromTestMethod_Minute() {
+            Assert.AreEqual("1m", MTikTimeUtil.ToMTikTime(60));
+        }
+
+        [TestMethod]
+        public void FromTestMethod_Hour() {
+            Assert.AreEqual("1h", MTikTimeUtil.ToMTikTime(3600));
+        }
+        [TestMethod]
+        public void FromTestMethod_Day() {
+            Assert.AreEqual("2d", MTikTimeUtil.ToMTikTime((3600 * 24 * 2)));
+        }
+
+        [TestMethod]
+        public void FromTestMethod_Week() {
+            Assert.AreEqual("1w", MTikTimeUtil.ToMTikTime((3600 * 24 * 7)));
+        }
+
+        [TestMethod]
+        public void FromTestMethod_AllFields() {
+            Assert.AreEqual("1w3d1h2m1s", MTikTimeUtil.ToMTikTime((1 + 120 + 3600 + 3600 * 24 * 10)));
+        }
+
+        [TestMethod]
+        public void FromTestMethod_Over1Year() {
+            Assert.AreEqual("71w3d", MTikTimeUtil.ToMTikTime((3600 * 24 * 500)));
+        }
+
+
+        [TestMethod]
+        public void ToTestMethod_Second() {
+            Assert.AreEqual(10, MTikTimeUtil.FromTikTime("10s"));
+        }
+        [TestMethod]
+        public void ToTestMethod_Minute() {
+            Assert.AreEqual(60, MTikTimeUtil.FromTikTime("1m"));
+        }
+
+        [TestMethod]
+        public void ToTestMethod_Hour() {
+            Assert.AreEqual(3600, MTikTimeUtil.FromTikTime("1h"));
+        }
+        [TestMethod]
+        public void ToTestMethod_Day() {
+            Assert.AreEqual((3600 * 24 * 2), MTikTimeUtil.FromTikTime("2d"));
+        }
+
+        [TestMethod]
+        public void ToTestMethod_Week() {
+            Assert.AreEqual((3600 * 24 * 7), MTikTimeUtil.FromTikTime("1w"));
+        }
+
+        [TestMethod]
+        public void ToTestMethod_AllFields() {
+            Assert.AreEqual((1 + 120 + 3600 + 3600 * 24 * 10), MTikTimeUtil.FromTikTime("1w3d1h2m1s"));
+        }
+
+        [TestMethod]
+        public void ToTestMethod_Over1Year() {
+            Assert.AreEqual((3600 * 24 * 500), MTikTimeUtil.FromTikTime("71w3d"));
+        }
+    }
+}
---
+++ tik4net/Utilities/MTikTimeUtil.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace VLife.Choreographer.Utilities {
+    /// <summary>
+    /// Functions to convert MikroTik timespans to useable formats.
+    /// </summary>
+    public static class MTikTimeUtil {
+        /// <summary>
+        /// Convert the seconds passed in to a MikroTik time string
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns>A string in the format the MikroTik expects for it's timespan fields</returns>
+        public static string ToMTikTime(int? seconds) {
+            return ToMTikTime((long)seconds);
+        }
+        /// <summary>
+        /// Convert the seconds passed in to a MikroTik time string
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns>A string in the format the MikroTik expects for it's timespan fields</returns>
+        public static string ToMTikTime(long? seconds) {
+            if (!seconds.HasValue || seconds == 0) return "0";
+            var t = TimeSpan.FromSeconds(seconds.Value);
+            var weeks = (long)t.TotalDays / 7;
+            t -= TimeSpan.FromDays(weeks * 7);
+            return
+                (weeks != 0 ? weeks + "w" : string.Empty) +
+                (t.Days != 0 ? t.Days + "d" : string.Empty) +
+                (t.Hours != 0 ? t.Hours + "h" : string.Empty) +
+                (t.Minutes != 0 ? t.Minutes + "m" : string.Empty) +
+                (t.Seconds != 0 ? t.Seconds + "s" : string.Empty);
+        }
+
+        /// <summary>
+        /// Convert a MikroTik time string to seconds
+        /// </summary>
+        /// <param name="time">The time as specified by MikroTik</param>
+        /// <returns></returns>
+        public static int FromTikTime(string time) {
+            // Sanitise the input
+            time = time.ToLower();
+            if (time == "none" || null == time) return 0;
+            int output = 0;
+            string[] split;
+            if ((split = time.Split('w')).Length >= 2) {
+                if (split.Length != 2) throw new FormatException("Multiple week sections specified");
+                output += int.Parse(split[0]) * 604800;
+                time = split[1];
+            }
+            if ((split = time.Split('d')).Length >= 2) {
+                if (split.Length != 2) throw new FormatException("Multiple day sections specified");
+                output += int.Parse(split[0]) * 86400;
+                time = split[1];
+            }
+            if ((split = time.Split('h')).Length >= 2) {
+                if (split.Length != 2) throw new FormatException("Multiple hour sections specified");
+                output += int.Parse(split[0]) * 3600;
+                time = split[1];
+            }
+            if ((split = time.Split('m')).Length >= 2) {
+                if (split.Length != 2) throw new FormatException("Multiple minute sections specified");
+                output += int.Parse(split[0]) * 60;
+                time = split[1];
+            }
+            if ((split = time.Split('s')).Length >= 2) {
+                if (split.Length != 2) throw new FormatException("Multiple second sections specified");
+                output += int.Parse(split[0]);
+                time = split[1];
+            }
+            return output;
+        }
+    }
+}
