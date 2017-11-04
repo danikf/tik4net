c4441925a4fcf246505b035ce472893ef5e56f4d
Ben Thompson <ben@invertedtomato.com>
2017-11-05 08:50:32 +1000

    Added more scan tests.

--- Tests/ScanTests.cs
+++ Tests/ScanTests.cs
@@ -1,5 +1,6 @@
 ﻿using InvertedTomato.TikLink;
 using InvertedTomato.TikLink.Records;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -27,7 +28,7 @@ namespace Tests {
         }
 
         [Fact]
-        public void Scan_LimitedProperties() {
+        public void Scan_Properties() {
             using (var link = Link.Connect(Credentials.Current.Host, Credentials.Current.Username, Credentials.Current.Password)) {
                 var result = link.Ip.Arp.Scan(new string[] { nameof(IpArp.MacAddress) });
                 foreach (var item in result) {
@@ -37,6 +38,28 @@ namespace Tests {
                     Assert.Null(item.Interface);
                 }
                 Assert.True(result.Count >= 1);
+            }
+        }
+        
+        [Fact]
+        public void Scan_Query() {
+            using (var link = Link.Connect(Credentials.Current.Host, Credentials.Current.Username, Credentials.Current.Password)) {
+                var result = link.Interfaces.Scan(null, new Dictionary<string, string>() { { nameof(IpArp.Id), "*1" } });
+                Assert.Equal(1, result.Count);
+                var eth1 = result.Single(a => a.Id == "*1");
+                Assert.Equal("*1", eth1.Id);
+                Assert.Equal("ether", eth1.Type);
+            }
+        }
+
+        [Fact]
+        public void Scan_Properties_Query() {
+            using (var link = Link.Connect(Credentials.Current.Host, Credentials.Current.Username, Credentials.Current.Password)) {
+                var result = link.Interfaces.Scan(new string[] { nameof(IpArp.Id) }, new Dictionary<string, string>() { { nameof(IpArp.Id), "*1" } });
+                Assert.Equal(1, result.Count);
+                var eth1 = result.Single(a => a.Id == "*1");
+                Assert.Equal("*1", eth1.Id);
+                Assert.Null(eth1.Type);
             }
         }
     }
