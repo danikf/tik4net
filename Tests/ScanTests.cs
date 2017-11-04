94d116073a12c0c15d5e4a3227169e81220d922b
Ben Thompson <ben@invertedtomato.com>
2017-11-04 16:32:00 +1000

    Fixed scan test.

--- Tests/ScanTests.cs
+++ Tests/ScanTests.cs
@@ -9,7 +9,7 @@ using System.Linq;
 namespace Tests {
     public class ScanTests {
         [Fact]
-        public void Scan() {
+        public void Scan_Interfaces() {
             using (var link = Link.Connect(Credentials.Current.Host, Credentials.Current.Username, Credentials.Current.Password)) {
                 var interfaces = link.Scan<Interface>();
                 Assert.True(interfaces.Count > 1);
@@ -17,6 +17,15 @@ namespace Tests {
                 var eth1 = interfaces.Single(a => a.DefaultName == "ether1");
                 Assert.Equal("*1", eth1.Id);
                 Assert.Equal("ether", eth1.Type);
+            }
+        }
+
+
+        [Fact]
+        public void Scan_IpArp() {
+            using (var link = Link.Connect(Credentials.Current.Host, Credentials.Current.Username, Credentials.Current.Password)) {
+                var ipArt = link.Scan<IpArp>();
+                Assert.True(ipArt.Count >= 1);
             }
         }
     }
