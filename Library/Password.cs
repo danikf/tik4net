697ae1c69ec87b69f09b91d4fb58c53fc6a0dc03
Ben Thompson <ben@invertedtomato.com>
2017-11-11 21:12:29 +1000

    Tidied Password implementation.

--- Library/Password.cs
+++ Library/Password.cs
@@ -5,25 +5,35 @@ using System.Text;
 
 namespace InvertedTomato.TikLink {
     public static class Password {
-        public static string Hash(string password, string challenge) { // TODO: Optimise
-            var hash_byte = new byte[challenge.Length / 2];
-            for (int i = 0; i <= challenge.Length - 2; i += 2) {
-                hash_byte[i / 2] = Byte.Parse(challenge.Substring(i, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+        public static string Hash(string password, string challenge) {
+            if (null == password) {
+                throw new ArgumentNullException(nameof(password));
+            }
+            if (null == challenge) {
+                throw new ArgumentNullException(nameof(challenge));
             }
 
-            var heslo = new byte[1 + password.Length + hash_byte.Length];
-            heslo[0] = 0;
-            Encoding.ASCII.GetBytes(password.ToCharArray()).CopyTo(heslo, 1);
-            hash_byte.CopyTo(heslo, 1 + password.Length);
+            // Decode hex challenge to byte array
+            var challengeBytes = new byte[challenge.Length / 2];
+            for (var i = 0; i <= challenge.Length - 2; i += 2) {
+                challengeBytes[i / 2] = Byte.Parse(challenge.Substring(i, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
 
-            byte[] hotovo;
+            // Concatenate password and challenge
+            var payload = new byte[1 + password.Length + challengeBytes.Length];
+            payload[0] = 0;
+            Encoding.ASCII.GetBytes(password.ToCharArray()).CopyTo(payload, 1);
+            challengeBytes.CopyTo(payload, 1 + password.Length);
+
+            // MD5 hash response
+            byte[] responseBytes;
             using (var md5 = MD5.Create()) {
-                hotovo = md5.ComputeHash(heslo);
+                responseBytes = md5.ComputeHash(payload);
             }
 
-            // Convert encoded bytes back to a 'readable' string
-            string result = "";
-            foreach (var h in hotovo) {
+            // Encode response in hex
+            string result = string.Empty;
+            foreach (var h in responseBytes) {
                 result += h.ToString("x2", CultureInfo.InvariantCulture);
             }
             return "00" + result;
