8e499c6062380082defd1d50e4d9f71781f0ac4e
Ben Thompson <ben@invertedtomato.com>
2017-11-20 18:56:39 +1000

    Renamed "UnknownProperties" to "OtherProperties".

--- Library/RecordReflection.cs
+++ Library/RecordReflection.cs
@@ -237,7 +237,7 @@ namespace InvertedTomato.TikLink {
                 }
             }
 
-            record.UnmatchedProperties = attributes;
+            record.OtherProperties = attributes;
         }
 
         /// <summary>
--- Library/Records/RecordBase.cs
+++ Library/Records/RecordBase.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace InvertedTomato.TikLink.Records {
     public class RecordBase {
-        public Dictionary<string, string> UnmatchedProperties;
+        [Obsolete("These properties may be removed at any time. Raise an issue at https://github.com/invertedtomato/tiklink/issues to have a property added properly.")]
+        public Dictionary<string, string> OtherProperties;
     }
 }
